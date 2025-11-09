37910e563b06eb8d82dfc2e542bbc166f349ac86
23-126 Dellon Ismu Enyunanda <dellonismu10@gmail.com>
2025-11-10 00:53:11 +0700

    Delete validasi.py

--- validasi.py
+++
@@ -1,8 +0,0 @@
-def get_identity():
-    return {
-        "nama": "Dellon Ismu Enyunanda",
-        "nim": "230411100126",
-        "program studi": "Teknik Informatika",
-        "fakultas": "Teknik",
-        "Universitas": "Universitas Trunojoyo Madura"
-    }
