76d4b13a169b4323af97161740ef1db5f7962a7d
23-126 Dellon Ismu Enyunanda <dellonismu10@gmail.com>
2025-11-10 00:30:58 +0700

    Update NIM in test_nim_ada function

--- test_validasi.py
+++ test_validasi.py
@@ -4,4 +4,5 @@ def test_nim_ada():
     with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
     # Ganti 210411100XXX dengan NIM kamu
-    assert re.search(r"210411100XXX", html), "NIM tidak ditemukan di halaman"
+    assert re.search(r"230411100126", html), "NIM tidak ditemukan di halaman"
+
