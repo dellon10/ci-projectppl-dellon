5e24ffa2c54085198253df6863374e4a8e5d9cd1
Dellon10 <dellonismu10.com>
2025-11-10 17:27:23 +0700

    Semua test validasi sudah passed

--- ci_projectppl_dellon/test_validasi.py
+++ ci_projectppl_dellon/test_validasi.py
@@ -1,27 +1,27 @@
 import re
-from ci_projectppl_dellon.validasi import get_identity
+from validasi import get_identity
 
 def test_nim():
-    with open("ci_projectppl_dellon/index.html", "r", encoding="utf-8") as f:
+    with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
-    assert re.search(r"230411100111", html), "NIM tidak ditemukan di halaman"
+    assert re.search(r"230411100126", html), "NIM tidak ditemukan di halaman"
 
 def test_nama():
-    with open("ci_projectppl_dellon/index.html", "r", encoding="utf-8") as f:
+    with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
-    assert re.search(r"Dellon Enyunanda", html), "Nama salah atau tidak ditemukan di halaman HTML"
+    assert re.search(r"Dellon Ismu Enyunanda", html), "Nama salah atau tidak ditemukan di halaman HTML"
 
 def test_prodi():
-    with open("ci_projectppl_dellon/index.html", "r", encoding="utf-8") as f:
+    with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
-    assert re.search(r"Teknika Informatika", html), "Nama prodi salah atau tidak ditemukan di halaman HTML"
+    assert re.search(r"Teknik Informatika", html), "Nama prodi salah atau tidak ditemukan di halaman HTML"
 
 def test_fakultas():
-    with open("ci_projectppl_dellon/index.html", "r", encoding="utf-8") as f:
+    with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
-    assert re.search(r"Teknika", html), "Nama fakultas salah atau tidak ditemukan di halaman HTML"
+    assert re.search(r"Teknik", html), "Nama fakultas salah atau tidak ditemukan di halaman HTML"
 
 def test_universitas():
-    with open("ci_projectppl_dellon/index.html", "r", encoding="utf-8") as f:
+    with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
-    assert re.search(r"Universitas Trunojoyo Maduraa", html), "Nama univ salah atau tidak ditemukan di halaman HTML"
+    assert re.search(r"Universitas Trunojoyo Madura", html), "Nama univ salah atau tidak ditemukan di halaman HTML"
