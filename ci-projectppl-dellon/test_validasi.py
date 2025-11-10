187f4aa68b12d6d9dc5eaa9de2dc8df5a91df882
Dellon10 <dellonismu10.com>
2025-11-10 12:03:33 +0700

    Tambah test validasi identitas mahasiswa

--- ci-projectppl-dellon/test_validasi.py
+++ ci-projectppl-dellon/test_validasi.py
@@ -5,3 +5,12 @@ def test_nim_ada():
         html = f.read()
     # Ganti 210411100XXX dengan NIM kamu
     assert re.search(r"230411100126", html), "NIM tidak ditemukan di halaman"
+
+def get_identity():
+    """Memastikan data identitas sesuai dengan validasi.py"""
+    data = validasi.get_identity()
+    assert data["nama"] == "Dellon Ismu Enyunanda"
+    assert data["nim"] == "230411100126"
+    assert data["program studi"] == "Teknik Informatika"
+    assert data["fakultas"] == "Teknik"
+    assert data["Universitas"] == "Universitas Trunojoyo Madura"
