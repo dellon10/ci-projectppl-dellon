a2de825d72943373f41e3a21207b09add4ae7499
Dellon10 <dellonismu10.com>
2025-11-12 16:45:52 +0700

    tes nama benar

--- ci-projectppl-dellon/test_validasi.py
+++ ci-projectppl-dellon/test_validasi.py
@@ -9,7 +9,7 @@ def test_nim():
 def test_nama():
     with open("index.html", "r", encoding="utf-8") as f:
         html = f.read()
-    assert re.search(r"Dellon Enyunanda", html), "Nama salah atau tidak ditemukan di halaman HTML"
+    assert re.search(r"Dellon Ismu Enyunanda", html), "Nama salah atau tidak ditemukan di halaman HTML"
 
 def test_prodi():
     with open("index.html", "r", encoding="utf-8") as f:
